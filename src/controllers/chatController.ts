29ffb1f76345abe95ad34675223e2ee193f17db7
Daniil Peretjaka <daniil.peretjaka@ivkhk.ee>
2025-11-25 11:22:00 +0200

    Added chatController.ts

---
+++ src/controllers/chatController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { chatModel } from "../models/chatMessage";
+
+// Controller that encapsulates interactions with the model and HTTP responses
+class ChatController {
+
+  public getMessages = (_req: Request, res: Response): void => {
+    const messages = chatModel.getAll();
+    res.json({ messages });
+  };
+}
+
+export const chatController = new ChatController();
